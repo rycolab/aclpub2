250929fecf3a36c902536ca60ee34ccdca28fb52
Jordan <zjzhang325@gmail.com>
2021-10-13 22:01:50 -0700

    Add more of the front matter

--- aclpub2/generate.py
+++ aclpub2/generate.py
@@ -4,8 +4,11 @@ from PyPDF2 import PdfFileReader
 import subprocess
 import yaml
 
-CONFERENCE_CFG_FILE = "conference_details.yml"
 PAPERS_CFG_FILE = "papers.yml"
+SPONSORS_CFG_FILE = "sponsors.yml"
+PREFACES_CFG_FILE = "prefaces.yml"
+
+
 PAPER_CMD = """
 \\addcontentsline{toc}{section}{TEMPLATE_TITLE}
 \pagestyle{fancy}
@@ -24,7 +27,7 @@ def generate(root: str):
     build_dir = Path("build")
     build_dir.mkdir(exist_ok=True)
 
-    conference, papers = load_configs(root)
+    conference, papers, sponsors, prefaces, organizing_committee = load_configs(root)
 
     # Load the proceedings template.
     with open(
@@ -53,10 +56,17 @@ def generate(root: str):
 
     template = template.replace("TEMPLATE_ABBREV", conference["abbreviation"])
     template = template.replace("TEMPLATE_CONFERENCE_NAME", conference["name"])
+    template = template.replace("TEMPLATE_ISBN", conference["isbn"])
     template = template.replace("TEMPLATE_YEAR", str(conference["start-date"].year))
     template = template.replace("TEMPLATE_TITLE", "ACL Anthology")
     template = template.replace(
         "TEMPLATE_CONFERENCE_DATES", get_conference_dates(conference)
+    )
+    template = template.replace("TEMPLATE_SPONSORS", generate_sponsors(sponsors, root))
+    template = template.replace("TEMPLATE_PREFACES", generate_prefaces(prefaces, root))
+    template = template.replace(
+        "TEMPLATE_ORGANIZING_COMMITTEE",
+        generate_organizing_committee(organizing_committee, root),
     )
 
     tex_file = Path(build_dir, "proceedings.tex")
@@ -78,15 +88,58 @@ def get_conference_dates(conference) -> str:
     return f"{start_month} {start_date.day} - {end_month} {end_date.day}"
 
 
+def generate_sponsors(sponsors, root: str) -> str:
+    output = ""
+    for level in sponsors:
+        output += "\\textbf{" + level["tier"] + "}\n\n\\bigskip"
+        for logo in level["logos"]:
+            output += (
+                "\includegraphics[width=2cm]{"
+                + str(Path(root, "sponsor-logos", logo))
+                + "}"
+            )
+        output += "\n\n\\bigskip "
+    return output
+
+
+def generate_prefaces(prefaces, root: str) -> str:
+    output = ""
+    for preface in prefaces:
+        output += "\\textbf{Preface by the " + preface["role"] + "}\\\\"
+        with open(Path(root, "prefaces", preface["file"])) as f:
+            body = f.read()
+            output += body
+        output += "\\newpage"
+    return output
+
+
+def generate_organizing_committee(organizing_committee, root: str) -> str:
+    output = ""
+    for entry in organizing_committee:
+        output += "\\textbf{" + entry["role"] + "}\\\\ "
+        for member in entry["members"]:
+            output += f"{member['name']}, {member['institution']}\\\\ "
+    return output
+
+
 def load_configs(root: str):
     """
     Loads all conference configuration files defined in the root directory.
     """
     # Conference Details
-    with open(Path(root, CONFERENCE_CFG_FILE), "r", encoding="utf-8") as f:
+    with open(Path(root, "conference-details.yml"), "r", encoding="utf-8") as f:
         conference = yaml.safe_load(f)
     # List of papers.
-    with open(Path(root, PAPERS_CFG_FILE), "r", encoding="utf-8") as f:
+    with open(Path(root, "papers.yml"), "r", encoding="utf-8") as f:
         papers = yaml.safe_load(f)
+    # Sponsors.
+    with open(Path(root, "sponsors.yml"), "r", encoding="utf-8") as f:
+        sponsors = yaml.safe_load(f)
+    # Prefaces.
+    with open(Path(root, "prefaces.yml"), "r", encoding="utf-8") as f:
+        prefaces = yaml.safe_load(f)
+    # Organizing Committee
+    with open(Path(root, "organizing-committee.yml"), "r", encoding="utf-8") as f:
+        organizing_committee = yaml.safe_load(f)
 
-    return conference, papers
+    return conference, papers, sponsors, prefaces, organizing_committee
