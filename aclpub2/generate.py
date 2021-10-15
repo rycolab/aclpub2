818cf9674dcabffc46e28fb12876a55601f70f6a
Jordan <zjzhang325@gmail.com>
2021-10-14 17:18:13 -0700

    Factor latex into .tex files for better readability

--- aclpub2/generate.py
+++ aclpub2/generate.py
@@ -4,24 +4,6 @@ from PyPDF2 import PdfFileReader
 import subprocess
 import yaml
 
-PAPERS_CFG_FILE = "papers.yml"
-SPONSORS_CFG_FILE = "sponsors.yml"
-PREFACES_CFG_FILE = "prefaces.yml"
-
-
-PAPER_CMD = """
-\\addcontentsline{toc}{section}{TEMPLATE_TITLE}
-\pagestyle{fancy}
-\cfoot{{\\thepage\\\\
-    \\footnotesize\\emph{Prooceedings of the TEMPLATE_CONFERENCE_NAME},
-    pages \\thepage -\\theptmp\\\\
-    TEMPLATE_CONFERENCE_DATES, TEMPLATE_YEAR
-    \\textcopyright TEMPLATE_YEAR Association for Computational Linguistics}}
-\setcounter{ptmp}{\\value{page} + TEMPLATE_NUM_PAGES - 1}
-\includepdf[pagecommand={\\thispagestyle{fancy}},pages=1]{TEMPLATE_PDF_PATH}
-\includepdf[pagecommand={\\thispagestyle{plain}},pages=2-TEMPLATE_NUM_PAGES]{TEMPLATE_PDF_PATH}
-"""
-
 
 def generate(root: str):
     build_dir = Path("build")
@@ -30,12 +12,8 @@ def generate(root: str):
     conference, papers, sponsors, prefaces, organizing_committee = load_configs(root)
 
     # Load the proceedings template.
-    with open(
-        Path(Path(__file__).parent, "proceedings_template.tex"),
-        "r",
-        encoding="utf-8",
-    ) as f:
-        template = f.read()
+    template = load_template("proceedings")
+    paper_template = load_template("paper_entry")
 
     # Generate the templated proceedings.tex file.
     def paper_cmd(paper):
@@ -43,7 +21,7 @@ def generate(root: str):
         pdf = PdfFileReader(pdf_path)
         title = paper["title"].replace("’", "'").replace("&", "\\&")
         return (
-            PAPER_CMD.replace("TEMPLATE_TITLE", title)
+            paper_template.replace("TEMPLATE_TITLE", title)
             .replace("TEMPLATE_PDF_PATH", pdf_path)
             .replace("TEMPLATE_NUM_PAGES", str(pdf.getNumPages()))
         )
@@ -78,6 +56,15 @@ def generate(root: str):
     subprocess.run(["pdflatex", f"-output-directory={build_dir}", str(tex_file)])
 
 
+def load_template(template: str) -> str:
+    with open(
+        Path(Path(__file__).parent, "templates", f"{template}.tex"),
+        "r",
+        encoding="utf-8",
+    ) as f:
+        return f.read()
+
+
 def get_conference_dates(conference) -> str:
     start_date = conference["start-date"]
     end_date = conference["end-date"]
@@ -90,35 +77,45 @@ def get_conference_dates(conference) -> str:
 
 def generate_sponsors(sponsors, root: str) -> str:
     output = ""
+    tier_template = load_template("sponsors_tier")
+    logo_template = load_template("sponsors_logo")
     for level in sponsors:
-        output += "\\textbf{" + level["tier"] + "}\n\n\\bigskip"
+        logos = ""
         for logo in level["logos"]:
-            output += (
-                "\includegraphics[width=2cm]{"
-                + str(Path(root, "sponsor-logos", logo))
-                + "}"
+            logos += logo_template.replace(
+                "TEMPLATE_LOGO_FILE", str(Path(root, "sponsor-logos", logo))
             )
-        output += "\n\n\\bigskip "
+        output += tier_template.replace("TEMPLATE_TIER", level["tier"]).replace(
+            "TEMPLATE_LOGOS", logos
+        )
     return output
 
 
 def generate_prefaces(prefaces, root: str) -> str:
     output = ""
+    template = load_template("preface")
     for preface in prefaces:
-        output += "\\textbf{Preface by the " + preface["role"] + "}\\\\"
         with open(Path(root, "prefaces", preface["file"])) as f:
             body = f.read()
-            output += body
-        output += "\\newpage"
+            output += template.replace("TEMPLATE_ROLE", preface["role"]).replace(
+                "TEMPLATE_BODY", body
+            )
     return output
 
 
 def generate_organizing_committee(organizing_committee, root: str) -> str:
     output = ""
+    role_template = load_template("organizing_committee_role")
+    entry_template = load_template("organizing_committee_entry")
     for entry in organizing_committee:
-        output += "\\textbf{" + entry["role"] + "}\\\\ "
+        entries = ""
         for member in entry["members"]:
-            output += f"{member['name']}, {member['institution']}\\\\ "
+            entries += entry_template.replace("TEMPLATE_NAME", member["name"]).replace(
+                "TEMPLATE_INSTITUTION", member["institution"]
+            )
+        output += role_template.replace("TEMPLATE_ROLE", entry["role"]).replace(
+            "TEMPLATE_ENTRIES", entries
+        )
     return output
 
 
