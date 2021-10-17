ef2811fc7e2e2fbff7eb6409e73ca3fd7c98f60a
Jordan <zjzhang325@gmail.com>
2021-10-16 21:39:07 -0700

    Finish jinja migration

--- aclpub2/generate.py
+++ aclpub2/generate.py
@@ -28,17 +28,18 @@ def generate(root: str):
     # Load the proceedings template.
     template = load_template("proceedings")
 
-    # Fill templates.
-    # paper_id_to_page = process_papers(
-    #     papers, Path(build_dir, "papers")
-    # )
+    process_papers(papers, root)
 
     rendered_template = template.render(
         root=str(root),
         conference=conference,
-        conference_dates=get_conference_dates(conference), sponsors=sponsors, prefaces=prefaces,
+        conference_dates=get_conference_dates(conference),
+        sponsors=sponsors,
+        prefaces=prefaces,
         organizing_committee=organizing_committee,
         program_committee=program_committee,
+        invited_talks=invited_talks,
+        papers=papers,
     )
 
     # Write the resulting tex file.
@@ -65,11 +66,6 @@ def get_conference_dates(conference) -> str:
 
 
 def process_papers(papers, root: Path):
-    paper_template = load_template("paper_entry")
-    toc_template = load_template("toc_entry")
-    templated_papers = ""
-    templated_toc = ""
-    page_ranges = {}
     page = 1
     for paper in papers:
         pdf_path = Path(root, "papers", f"{paper['id']}.pdf")
@@ -83,100 +79,10 @@ def process_papers(papers, root: Path):
             ]
         )
         pdf = PdfFileReader(str(pdf_path))
-        title = paper["title"].replace("’", "'").replace("&", "\\&")
-        templated_papers += (
-            paper_template.replace("TEMPLATE_TITLE", title)
-            .replace("TEMPLATE_PDF_PATH", str(pdf_path))
-            .replace("TEMPLATE_NUM_PAGES", str(pdf.getNumPages()))
-        )
-        author_string = ""
-        for i, author in enumerate(paper["authors"]):
-            if i == len(paper["authors"]) - 1:  # Last author
-                author_string += " and "
-            author_string += author
-            if i < len(paper["authors"]) - 1:
-                author_string += ", "
-        templated_toc += (
-            toc_template.replace("TEMPLATE_TITLE", title)
-            .replace("TEMPLATE_AUTHORS", author_string)
-            .replace("TEMPLATE_PAGE", str(page))
-        )
-        page_ranges[paper["id"]] = (page, page + pdf.getNumPages() - 1)
+        paper["title"] = paper["title"].replace("’", "'").replace("&", "\\&")
+        paper["num_pages"] = pdf.getNumPages()
+        paper["page_range"] = (page, page + pdf.getNumPages() - 1)
         page += pdf.getNumPages()
-    return page_ranges
-
-
-def generate_organizing_committee(organizing_committee) -> str:
-    output = ""
-    role_template = load_template("committee_role")
-    for entry in organizing_committee:
-        entries = ""
-        for member in entry["members"]:
-            entries += generate_committee_entry(member)
-        output += role_template.replace("TEMPLATE_ROLE", entry["role"]).replace(
-            "TEMPLATE_ENTRIES", entries
-        )
-    return output
-
-
-def generate_committee_entry(member) -> str:
-    entry_template = load_template("committee_entry")
-    return entry_template.replace("TEMPLATE_NAME", member["name"]).replace(
-        "TEMPLATE_INSTITUTION", member["institution"]
-    )
-
-
-def generate_program_committee(program_committee) -> str:
-    output = ""
-    program_committee_template = load_template("program_committee")
-
-    # First add program commitee members
-    program_committee_entries = ""
-    for member in program_committee["program_committee"]:
-        program_committee_entries += generate_committee_entry(member)
-
-    area_chair_role_template = load_template("area_chair_role")
-    senior_area_chairs = ""
-    for area in program_committee["senior_area_chairs"]:
-        entries = ""
-        for member in area["members"]:
-            entries += generate_committee_entry(member)
-        senior_area_chairs += area_chair_role_template.replace("TEMPLATE_AREA",
-                                                               area["area"]).replace("TEMPLATE_ENTRIES", entries)
-    area_chairs = ""
-    for area in program_committee["area_chairs"]:
-        entries = ", ".join(area["members"])
-        area_chairs += area_chair_role_template.replace("TEMPLATE_AREA",
-                                                        area["area"]).replace("TEMPLATE_ENTRIES", entries)
-
-    output = program_committee_template.replace(
-        "TEMPLATE_PROGRAM_COMMITTEE_ENTRIES", program_committee_entries
-    ).replace("TEMPLATE_SENIOR_AREA_CHAIRS", senior_area_chairs).replace("TEMPLATE_AREA_CHAIRS", area_chairs).replace("TEMPLATE_REVIEWERS", ", ".join(program_committee["reviewers"]))
-    return output
-
-
-def generate_invited_talks(invited_talks, root: Path) -> str:
-    output = ""
-    invited_talk_template = load_template("invited_talk")
-    for talk in invited_talks:
-        id = talk["id"]
-        with open(Path(root, "invited_talks", f"{id}_abstract.tex")) as f:
-            abstract = f.read()
-        with open(Path(root, "invited_talks", f"{id}_bio.tex")) as f:
-            bio = f.read()
-        output += (
-            invited_talk_template.replace("TEMPLATE_NAME", talk["speaker_name"])
-            .replace("TEMPLATE_INSTITUTION", talk["institution"])
-            .replace("TEMPLATE_TITLE", talk["title"])
-            .replace("TEMPLATE_ABSTRACT", abstract)
-            .replace("TEMPLATE_BIO", bio)
-        )
-    return output
-
-
-def generate_program(program) -> str:
-    program = get_program_sessions_by_date(program)
-    return "PLACEHOLDER"
 
 
 def get_program_sessions_by_date(program):
--- aclpub2/templates.py
+++ aclpub2/templates.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from pathlib import Path
 from typing import List, Any
 
@@ -11,8 +12,25 @@ def load_file(*args: str):
         return f.read()
 
 
-def join(delimiter: str, items: List[Any]):
+def join(delimiter: str, items: List[Any], delimiter_last=None):
+    if delimiter_last:
+        output = delimiter.join(items[:-1])
+        output = output + delimiter_last + items[-1]
+        return output
     return delimiter.join(items)
+
+
+def group_by_last_name(names: List[str]) -> List[List[str]]:
+    alphabetized_names = defaultdict(list)
+    for name in names:
+        last_name = name.split(" ")[-1]
+        alphabetized_names[last_name[0].lower()].append(name)
+    output = []
+    letters = list(alphabetized_names.keys())
+    letters.sort()
+    for letter in letters:
+        output.append(alphabetized_names[letter])
+    return output
 
 
 LATEX_JINJA_ENV = jinja2.Environment(
@@ -26,7 +44,7 @@ LATEX_JINJA_ENV = jinja2.Environment(
     autoescape=False,
     loader=jinja2.FileSystemLoader(str(TEMPLATE_DIR))
 )
-LATEX_JINJA_ENV.globals.update(load_file=load_file, join=join)
+LATEX_JINJA_ENV.globals.update(load_file=load_file, join=join, group_by_last_name=group_by_last_name)
 
 
 def load_template(template: str) -> jinja2.Template:
